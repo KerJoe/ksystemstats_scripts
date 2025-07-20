2dc0ac60a6778ee559eed5652d948bfc59925d0d
KerJoe <KerJoe@0xfabaceda.com>
2025-07-20 13:48:07 +0400

    Handle errors of 'nvidia-smi' in example script

--- example.py
+++ example.py
@@ -11,12 +11,15 @@ while True:
         print("gpu_fan_speed\tfrandom")
     elif req[0] == "gpu_fan_speed":
         if (req[1] == "value"):
-            stdout = subprocess.run(["nvidia-smi", "-q"], check=True, stdout=subprocess.PIPE).stdout.decode('utf-8')
-            for string in stdout.split("\n"):
-                if "Fan Speed" in string:
-                    print(string.split(":")[1].replace("%", "").replace(" ", ""))
-                    break
-            else:
+            try:
+                stdout = subprocess.run(["nvidia-smi", "-q"], check=True, stdout=subprocess.PIPE).stdout.decode('utf-8')
+                for string in stdout.split("\n"):
+                    if "Fan Speed" in string:
+                        print(string.split(":")[1].replace("%", "").replace(" ", ""))
+                        break
+                else:
+                    print()
+            except Exception:
                 print()
         elif (req[1] == "min"):
             print(0)
